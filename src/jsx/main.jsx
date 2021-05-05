fb31502d2964584947a15336464a852f2b10aa9b
Yegor Bitensky <yegorbitensky@gmail.com>
2021-05-05 16:47:57 +0300

    Add field fill square method, move field create empty row logic to method, add log field matrix method

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 
 
-class FieldFilledSpace {
+class Field {
 
   /**
    * Creation.
@@ -10,27 +10,57 @@ class FieldFilledSpace {
    * @param {number} height - Field height.
    */
   constructor(width, height) {
+    this._height = height;
     this._matrix = [];
     for (let i = 0; i < width; i++) {
-      let row = [];
-      for (let j = 0; j < height; j++) {
-        row.push(false);
-      }
-      this._matrix.push(row);
+      this._matrix.push(this._createEmptyRow());
     }
   }
 
   /**
    * Get matrix.
-   * @returns {number[][]} Matrix.
+   * @returns {boolean[][]} Matrix.
    */
   get matrix() {
     return this._matrix;
   }
+
+  /**
+   * Fill square.
+   * @param {number} x - X square coordinate.
+   * @param {number} y - Y square coordinate.
+   */
+  fillSquare(x, y) {
+    this._matrix[x][y] = true;
+    if (!this._matrix[x].some(s => !s)) {
+      this._matrix.splice(x, 1);
+      this._matrix.splice(0, 0, this._createEmptyRow());
+    }
+  }
+
+  /**
+   * Create empty row.
+   * @returns {boolean[]} - Field row fill data.
+   * @private
+   */
+  _createEmptyRow() {
+    let row = [];
+    for (let i = 0; i < this._height; i++) {
+      row.push(false);
+    }
+    return row;
+  }
+
+  /** Show matrix in console. */
+  logMatrix() {
+    for (let row of this._matrix) {
+      console.log(row.map(s => s? 1 : 0));
+    }
+  }
 }
 
 
-window.ffs = new FieldFilledSpace(10, 10);
+window.field = new Field(3, 3);
 
 
 class HelloMessage extends React.Component {
